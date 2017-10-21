57bab22a52e0f162054172e5a07430afc912b804
Bailey Seymour (atomikpanda) <b.seymour@me.com>
2017-10-21 16:21:43 -0400

    Initial Commit

---
+++ APColorPicker/APColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  APColorPicker.h
+//  APColorPicker
+//
+//  Created by Bailey Seymour on 10/21/17.
+//  Copyright © 2017 Bailey Seymour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APColorPicker.
+FOUNDATION_EXPORT double APColorPickerVersionNumber;
+
+//! Project version string for APColorPicker.
+FOUNDATION_EXPORT const unsigned char APColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APColorPicker/PublicHeader.h>
+
+
